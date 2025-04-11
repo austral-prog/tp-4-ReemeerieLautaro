0d928b891b22bb5e5a2b2a37a0bae0c40ff7740d
ReemeerieLautaro <lreemeerie@mail.austral.edu.ar>
2025-04-11 11:49:10 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,26 +1,26 @@
+import math
 def line():
     print("TO DO")
-    import math
-    
-    CofA= float(input("Ingrese el coeficiente A: "))
-    CofB= float(input("Ingrese el coeficiente B: "))
-    CofX1= float(input("Ingrese el coeficiente X1: "))
-    CofX2= float(input("Ingrese el coeficiente X2: "))
-    
-    print (f"El coeficiente A de su ecuación de la recta es:{CofA}")
-    print (f"El coeficiente B de su ecuación de la recta es:{CofB}")
-    print (f"El coeficiente X1 de su ecuación de la recta es:{CofX1}")
-    print (f"El coeficiente X2 de su ecuación de la recta es:{CofX2}")
-    
-    print("Para la siguiente ecuación:")
-    print(f"Y={CofA}X+{CofB}")
-    
-    Y1= CofA*CofX1+CofB
-    Y2= CofA*CofX2+CofB
+    A= float(input("Ingrese el coeficiente A: "))
+    B= float(input("Ingrese el coeficiente B: "))
+    X1= float(input("Ingrese el coeficiente X1: "))
+    X2= float(input("Ingrese el coeficiente X2: "))
+
+    print (f"El coeficiente A de su ecuación de la recta es:{A}")
+    print (f"El coeficiente B de su ecuación de la recta es:{B}")
+    print (f"El coeficiente X1 de su ecuación de la recta es:{X1}")
+    print (f"El coeficiente X2 de su ecuación de la recta es:{X2}")
+
+    print("\nPara la siguiente ecuación:")
+    print(f"\tY={A}X+{B}")
+
+    Y1= A*X1+B
+    Y2= A*X2+B
     print("Dados los siguientes puntos:")
-    print (f"P1 ({CofX1};{Y1})")
-    print(f"P2 ({CofX2};{Y2})")
-    
-    Distancia= math.sqrt((CofX2-CofX1)**2 + (Y2-Y1)**2)
+    print (f"P1 ({X1};{Y1})")
+    print(f"P2 ({X2};{Y2})")
+
+    Distancia= math.sqrt((X2-X1)**2 + (Y2-Y1)**2)
     print(f"La distancia entre ellos es:{Distancia}")
+
 line()
